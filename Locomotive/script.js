709e5c611948d7305ef65f044987b76da8ec3d74
Harsh Ninave <harshninave58@gmail.com>
2024-02-17 22:52:39 +0530

    Comments added as an instructions to follow

--- Locomotive/script.js
+++ Locomotive/script.js
@@ -1,3 +1,6 @@
+// Copied from https://github.com/locomotivemtl/locomotive-scroll
+// Inside README.md file there is "Smooth" Section which contains some code snippets that are used here
+
 const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
